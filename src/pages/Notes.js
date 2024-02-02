0d8632c4038e274dfeb8a6a58545c05341338087
harshal <harshalc562@gmail.com>
2024-02-02 10:45:00 +0530

    Mui Grid done

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import { Grid, Paper } from '@mui/material'
+
 const Notes = () => {
   const [notes, setNotes] = useState([])
   useEffect(() => {
@@ -8,9 +10,27 @@ const Notes = () => {
   }, [])
   return (
     <>
+    {/* <Grid container>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper>1</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper>2</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper>3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper>4</Paper>
+      </Grid>
+    </Grid> */}
+    <Grid container>
       {notes.map(note => (
-        <p id={note.id}>{note.title}</p>
+        <Grid item key={note.id} xs={12} md={6} lg={4}>
+          <Paper>{note.title}</Paper>
+        </Grid>
       ))}
+    </Grid>
     </>
   )
 }
